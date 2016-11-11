fe9e073fea977705ee63a61ad08a98f2423b066e
yoichi kobayashi <info@tplh.net>
2016-11-11 17:04:58 +0900

    [modify] changed shape of points to circle.

--- src/glsl/points.fs
+++ src/glsl/points.fs
@@ -6,6 +6,10 @@ uniform sampler2D texNext;
 varying vec2 vUv;
 
 void main(void) {
+  vec3 n;
+  n.xy = gl_PointCoord * 2.0 - 1.0;
+  n.z = 1.0 - dot(n.xy, n.xy);
+  if (n.z < 0.0) discard;
   vec4 color = texture2D(texPrev, vUv);
   gl_FragColor = color;
 }
